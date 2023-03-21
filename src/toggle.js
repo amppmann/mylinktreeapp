d68cf5eac6dd86fa02e6503446f764d72b2bb437
sachindsilva16 <sachindsilva@gmail.com>
2023-03-21 20:24:03 +0530

    committed all files

---
+++ src/toggle.js
@@ -0,0 +1,9 @@
+// document.getElementById('toggle').addEventListener('change', function () {
+//     if (this.checked) {
+//         document.documentElement.classList.add('dark');
+//     }
+//     else {
+
+//         document.documentElement.classList.remove('dark');
+//     }
+// })
